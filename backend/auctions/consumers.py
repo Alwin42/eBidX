24dc64db75b679cd3b93dc0d9001fd2eea984d97
ALEN T L <alentl5000@gmail.com>
2026-03-27 04:27:27 +0530

    Check user anonymity with group_name

--- backend/auctions/consumers.py
+++ backend/auctions/consumers.py
@@ -1,10 +1,11 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
+from channels.db import database_sync_to_async
 
 
 class AuctionConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.auction_id = self.scope["url_route"]["kwargs"]["id"]
+        self.auction_id = self.scope["url_route"]["kwargs"]["pk"]
         self.room_group_name = f"auction_{self.auction_id}"
 
         await self.channel_layer.group_add(self.room_group_name, self.channel_name)
@@ -31,16 +32,13 @@ class NotificationConsumer(AsyncWebsocketConsumer):
 
         if not self.user or self.user.is_anonymous:
             await self.close()
-
         else:
             self.group_name = f"user_{self.user.id}"
-
             await self.channel_layer.group_add(self.group_name, self.channel_name)
-
             await self.accept()
 
     async def disconnect(self, close_code):
-        if self.user and not self.user.is_anonymous:
+        if hasattr(self, "group_name"):
             await self.channel_layer.group_discard(self.group_name, self.channel_name)
 
     async def send_notification(self, event):
