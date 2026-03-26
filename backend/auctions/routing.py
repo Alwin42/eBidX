a32f4888b4dfda16d03f3d3254df900ffe382373
ALEN T L <alentl5000@gmail.com>
2026-03-27 04:30:18 +0530

    Changed routing with attribute name to attribute constraint (id -> pk)

--- backend/auctions/routing.py
+++ backend/auctions/routing.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r"ws/auction/(?P<id>\d+)/$", consumers.AuctionConsumer.as_asgi()),
+    re_path(r"ws/auction/(?P<pk>\d+)/$", consumers.AuctionConsumer.as_asgi()),
     re_path(r"ws/notifications/$", consumers.NotificationConsumer.as_asgi()),
 ]
